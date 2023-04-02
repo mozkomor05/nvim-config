7ec75fd3f9419aa1a8cb8bf90c79a46b48d1ee7c
David Moškoř <david.moskor@apertia.cz>
2023-04-02 18:49:46 +0200

    fixes

--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -1,20 +1,16 @@
 local function open_nvim_tree(data)
-	-- buffer is a [No Name]
-	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
 	-- buffer is a directory
 	local directory = vim.fn.isdirectory(data.file) == 1
 
-	if not no_name and not directory then
+	if not directory then
 		return
 	end
 
-	if directory then
-		vim.cmd.cd(data.file)
-	end
+	-- switch to directory
+	vim.cmd.cd(data.file)
 
-	-- open the tree, find the file but don't focus it
-	require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+	-- open the tree
+	require('nvim-tree.api').tree.open()
 end
 
 vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
@@ -26,16 +22,21 @@ return {
 	},
 	cond = not vim.g.started_by_firenvim,
 	keys = {
-		{ '<leader>nf', vim.cmd.NvimTreeFocus,                                            desc = '[N]vimTree [F]ocus' },
-		{ '<leader>no', vim.cmd.NvimTreeFindFile,                                         desc = '[N]vimTree [O]pen current file' },
-		{ '<leader>nt', function() require('nvim-tree.api').tree.toggle(false, true) end, desc = '[N]vimTree [T]oggle' },
+		{ '<leader>nf', vim.cmd.NvimTreeFocus,												desc = '[N]vimTree [F]ocus' },
+		{ '<leader>no', vim.cmd.NvimTreeFindFile,											desc = '[N]vimTree [O]pen current file' },
+		{ '<leader>nt', function() require('nvim-tree.api').tree.toggle(false, true) end,	desc = '[N]vimTree [T]oggle' },
 	},
 	config = function()
-		local lib = require("nvim-tree.lib")
+		local lib = require('nvim-tree.lib')
 		local api = require('nvim-tree.api')
 
 		local git_add = function()
 			local node = lib.get_node_at_cursor()
+
+			if not node then
+				return
+			end
+
 			local gs = node.git_status.file
 
 			-- If the file is untracked, unstaged or partially staged, we stage it
@@ -52,47 +53,17 @@ return {
 		local on_attach = function(bufnr)
 			api.config.mappings.default_on_attach(bufnr)
 
-			vim.keymap.set('n', 'ga', git_add, { buffer = bufnr })
-		end
+			local function opts(desc)
+				return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
 
+			vim.keymap.set('n', 'ga', git_add, opts('Git Add'))
+		end
 
 		require('nvim-tree').setup({
 			on_attach = on_attach,
-		})
-
-		local function tab_win_closed(winnr)
-			local tabnr = vim.api.nvim_win_get_tabpage(winnr)
-			local bufnr = vim.api.nvim_win_get_buf(winnr)
-			local buf_info = vim.fn.getbufinfo(bufnr)[1]
-			local tab_wins = vim.tbl_filter(function(w) return w ~= winnr end, vim.api.nvim_tabpage_list_wins(tabnr))
-			local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
-			if buf_info.name:match(".*NvimTree_%d*$") then -- close buffer was nvim tree
-				-- Close all nvim tree on :q
-				if not vim.tbl_isempty(tab_bufs) then -- and was not the last window (not closed automatically by code below)
-				api.tree.close()
-				end
-			else                                         -- else closed buffer was normal buffer
-				if #tab_bufs == 1 then                   -- if there is only 1 buffer left in the tab
-					local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
-					if last_buf_info.name:match(".*NvimTree_%d*$") then -- and that buffer is nvim tree
-						vim.schedule(function()
-							if #vim.api.nvim_list_wins() == 1 then -- if its the last buffer in vim
-								vim.cmd.quit()
-							else                         -- else there are more tabs open
-								vim.api.nvim_win_close(tab_wins[1], true) -- then close only the tab
-							end
-						end)
-					end
-				end
-			end
-		end
-
-		vim.api.nvim_create_autocmd('WinClosed', {
-			callback = function()
-				local winnr = tonumber(vim.fn.expand("<amatch>"))
-				vim.schedule_wrap(tab_win_closed(winnr))
-			end,
-			nested = true
+			disable_netrw = false,
+			hijack_netrw = true,
 		})
 	end
 }
