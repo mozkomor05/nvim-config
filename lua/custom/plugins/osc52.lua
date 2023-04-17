9b477d694a275bf0d1942f56dd6a58459eeb0cc8
David Moškoř <david.moskor@apertia.cz>
2023-04-17 22:37:31 +0200

    removed redundant conflicting keymaps for osc52

--- lua/custom/plugins/osc52.lua
+++ lua/custom/plugins/osc52.lua
@@ -7,10 +7,6 @@ return {
 			trim = false,
 		})
 
-		vim.keymap.set("n", "<leader>c", require("osc52").copy_operator, { expr = true })
-		vim.keymap.set("n", "<leader>cc", "<leader>c_", { remap = true })
-		vim.keymap.set("x", "<leader>c", require("osc52").copy_visual)
-
 		vim.api.nvim_create_autocmd('TextYankPost', {
 			callback = function()
 				if vim.v.event.operator == 'y' and vim.env.SSH_CONNECTION then
