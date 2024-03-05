790124fb503bc1905082b9da24e893439923ed88
David Moškoř <44263817+mozkomor05@users.noreply.github.com>
2024-03-05 14:14:30 +0100

    split keymaps to separate file

--- init.lua
+++ init.lua
@@ -16,6 +16,8 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
 
+vim.opt.termguicolors = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
@@ -58,38 +60,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
-
--- Remap for toggling relative line numbers
-vim.keymap.set('n', '<leader>rl', ':set rnu!<cr>', { desc = '[R]elative [Line] numbers' })
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Keymaps
+require 'custom.keymaps'
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -17,6 +17,35 @@ return {
       },
     }
 
+    local api = require 'nvim-tree.api'
+
+    local git_add = function()
+      local node = api.tree.get_node_under_cursor()
+      local gs = node.git_status.file
+      local dir = node.git_status.dir
+
+      -- If the current node is a directory get children status
+      if gs == nil and dir ~= nil then
+        gs = (dir.direct ~= nil and dir.direct[1]) or (dir.indirect ~= nil and dir.indirect[1])
+      end
+
+      if gs == nil then
+        return
+      end
+
+      -- If the file is untracked, unstaged or partially staged, we stage it
+      if gs == '??' or gs == 'MM' or gs == 'AM' or gs == ' M' then
+        vim.cmd('silent !git add ' .. node.absolute_path)
+
+      -- If the file is staged, we unstage
+      elseif gs == 'M ' or gs == 'A ' then
+        vim.cmd('silent !git restore --staged ' .. node.absolute_path)
+      end
+
+      api.tree.reload()
+    end
+
+    vim.keymap.set('n', 'ga', git_add, { desc = '[G]it toggle [A]dd file' })
     vim.keymap.set('n', '<leader>nf', vim.cmd.NvimTreeFocus, { desc = '[N]vimTree [F]ocus' })
     vim.keymap.set('n', '<leader>no', vim.cmd.NvimTreeFindFile, { desc = '[N]vimTree [O]pen current file' })
     vim.keymap.set('n', '<leader>nt', function()
