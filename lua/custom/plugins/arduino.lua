329ea332b2b07221faf8c9d9369b4573d644838e
David Moškoř <44263817+mozkomor05@users.noreply.github.com>
2025-03-09 10:54:02 +0100

    osc52 removed because of native support

--- lua/custom/plugins/arduino.lua
+++ lua/custom/plugins/arduino.lua
@@ -9,7 +9,12 @@ return {
       vim.keymap.set('n', keys, func, { desc = desc })
     end
 
-    nmap('<leader>ar', '<cmd>ArduinoUpload<cr>', 'Upload')
+    nmap('<leader>aa', '<cmd>ArduinoAttach<cr>', 'Attach')
+    nmap('<leader>av', '<cmd>ArduinoVerify<cr>', 'Verify')
+    nmap('<leader>au', '<cmd>ArduinoUpload<cr>', 'Upload')
+    nmap('<leader>aus', '<cmd>ArduinoUploadAndSerial<cr>', 'Upload and Serial')
     nmap('<leader>as', '<cmd>ArduinoSerial<cr>', 'Serial')
+    nmap('<leader>ab', '<cmd>ArduinoChooseBoard<cr>', 'Choose Board')
+    nmap('<leader>ap', '<cmd>ArduinoChooseProgrammer<cr>', 'Choose Programmer')
   end,
 }
